72133efb5a02854eb6e640edb81457e14622e265
Jay D'Aiuto <93616573+jdaiuto29@users.noreply.github.com>
2021-12-17 16:48:41 -0500

    modified playlist.js  to render playlist items

--- javascript/playlist.js
+++ javascript/playlist.js
@@ -1,25 +1,42 @@
 let playlist = JSON.parse(localStorage.getItem('playlist'));
 
-
-function renderSongs(playlist) {
-
-    const playlistArray = playlist.map(function(currentSong) {
-
+function printHtmlAlbums(albumArray, trackArray, parentTag) {
+    const albumHtmlArray = albumArray.map(album => {
+        const date = album.released.substring(0, 4);
+        const albumTracks = trackArray.filter(track => track.albumId == album.id);
+        const trackHtmlArray = albumTracks.map(track => `<li class="tracklist-item">${track.index}. "${track.name}"</li>`);
         return `
-    <div class = 'col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4' id = 'movie-div'>
-    <img class = 'w-100 border border-white' id = 'album-cover' src='${album cover placeholder}'/>
-    <div class = 'movie-info'>
-    <div class = 'd-flex justify-content-between'><h6>${album title}</h6>
-    <div id = 'movie-year'>${song title}</div>
-    </div>
-    <div class = "mt-3"><button class = 'add-movie btn btn-primary btn-sm d-flex justify-content-start' data-imdbid= ${currentMovie.imdbID}>Remove Movie</button></div>
-    </div>
-    </div>
-    `
 
+      <div class = 'col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4' id = 'playlist-div'>
+      <img class="album-cover" src="${getAlbumArtwork(album.id)}" alt="${album.name} Album Cover">
+      </div>
+      <div class = 'song-info'>
+        <div class="card-header">
+          <span class="card-header-item">Album: ${album.name}&nbsp<em>(${date})</em></span>
+          <span class="card-header-item">Artist: ${album.artistName}</span>
+          <span class="card-header-item">Track: ${track.index}. "${track.name}</span>
+        </div>
+      `
     });
-    const savedSongs = document.querySelector('#song-container');
-    savedSong.innerHTML = playlistArray.join('');
+    document.querySelector(parentTag).innerHTML = albumHtmlArray.join('');
 }
 
-renderMovies(playlist);
+printHtmlAlbums(parameters, needed, here);
+
+// this is the function to save album and track data to local storage (just needs proper parameters) -- THIS NEEDS TO GO INTO THE INDEX.JS in order and be connected to an "Add Button" when data is rendered
+
+// printHtmlAlbums(parameters, needed, here);
+
+// function saveToPlayList(trackID) {
+//     const track = trackData.find(function(currentTrack) {
+//         return currentTrack.trackID == trackID;
+//     });
+//     let playlistJSON = localStorage.getItem('playlist');
+//     let playlist = JSON.parse(playlistJSON);
+
+//     if (playlist === null) {
+//         playlist = [];
+//     }
+//     playlist.push(track);
+//     playlistJSON = JSON.stringify(playlist);
+//     localStorage.setItem('playlist', playlistJSON);
