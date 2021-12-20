dff671b3b4cffad6ac596af60d25f50267541e4d
Jay D'Aiuto <93616573+jdaiuto29@users.noreply.github.com>
2021-12-20 15:49:16 -0500

    took out unneeded console logs

--- javascript/lyrics.js
+++ javascript/lyrics.js
@@ -2,16 +2,15 @@ const mainContainer = document.getElementById("myData");
 
 function renderLyrics(currentLyrics) {
 
-    mainContainer.innerHTML = currentLyrics.lyrics.substring(currentLyrics.lyrics.indexOf('\n') + 1).replaceAll('\r', '<br>').replace('Paroles de la chanson ', '').replace('par ', '').replaceAll('\n', '<br>');
+    mainContainer.innerHTML = currentLyrics.lyrics.substring(currentLyrics.lyrics.indexOf('\n') + 1).replaceAll('\r', '<br>').replace('Paroles de la chanson ', '').replace('par ', '').replaceAll('\n', '<br>').replaceAll('[]', '');
 
 };
 
-fetch('https://api.lyrics.ovh/v1/michael jackson/billie jean')
+fetch('https://api.lyrics.ovh/v1/stone temple pilots/plush')
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        console.log(data)
         renderLyrics(data);
     })
     .catch(function(err) {
